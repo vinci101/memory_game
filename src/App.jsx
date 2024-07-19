d99d817e777c2f7aeee7be523290c7eec931c37a
alvin calma <alvin.l.calma.ac@gmail.com>
2024-07-19 17:17:51 +0800

    flip logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 const cardImg = [
-  { src: "./img/plane.png" }, //<a href="https://www.flaticon.com/free-icons/message" title="message icons">Message icons created by Freepik - Flaticon</a>
-  { src: "./img/heart.png" }, //<a href="https://www.flaticon.com/free-icons/heart" title="heart icons">Heart icons created by Freepik - Flaticon</a>
-  { src: "./img/bat.png" }, //<a href="https://www.flaticon.com/free-icons/paper" title="paper icons">Paper icons created by Freepik - Flaticon</a>
-  { src: "./img/fish.png" }, //<a href="https://www.flaticon.com/free-icons/art-and-design" title="art and design icons">Art and design icons created by Freepik - Flaticon</a>
-  { src: "./img/elephant.png" }, //<a href="https://www.flaticon.com/free-icons/art-and-design" title="art and design icons">Art and design icons created by Freepik - Flaticon</a>
-  { src: "./img/butterfly.png" }, //<a href="https://www.flaticon.com/free-icons/art" title="art icons">Art icons created by Freepik - Flaticon</a>
-]; //<a href="https://www.flaticon.com/free-icons/origami" title="origami icons">Origami icons created by Freepik - Flaticon</a>
+  { src: "./img/plane.png", matched: false }, //<a href="https://www.flaticon.com/free-icons/message" title="message icons">Message icons created by Freepik - Flaticon</a>
+  { src: "./img/heart.png", matched: false }, //<a href="https://www.flaticon.com/free-icons/heart" title="heart icons">Heart icons created by Freepik - Flaticon</a>
+  { src: "./img/bat.png", matched: false }, //<a href="https://www.flaticon.com/free-icons/paper" title="paper icons">Paper icons created by Freepik - Flaticon</a>
+  { src: "./img/fish.png", matched: false }, //<a href="https://www.flaticon.com/free-icons/art-and-design" title="art and design icons">Art and design icons created by Freepik - Flaticon</a>
+  { src: "./img/elephant.png", matched: false }, //<a href="https://www.flaticon.com/free-icons/art-and-design" title="art and design icons">Art and design icons created by Freepik - Flaticon</a>
+  { src: "./img/butterfly.png", matched: false }, //<a href="https://www.flaticon.com/free-icons/art" title="art icons">Art icons created by Freepik - Flaticon</a>
+]; //
 
 const App = () => {
   const [cards, setCards] = useState([]);
@@ -27,19 +27,56 @@ const App = () => {
   };
 
   const handleChoice = (card) => {
-    console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 500);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+  console.log(cards);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
   };
 
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      {/* <img src={"./img/plane.png"} alt="img" /> */}
       <button onClick={shuffleCard}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <Card card={card} key={card.id} handleChoice={handleChoice} />
+          <Card
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
+      <a
+        href="https://www.flaticon.com/free-icons/origami"
+        title="origami icons"
+        className="att"
+      >
+        Icons created by Freepik - Flaticon
+      </a>
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable react/prop-types */
 import "./cardStyle.css";
 
-const Card = ({ card, handleChoice }) => {
+const Card = ({ card, handleChoice, flipped }) => {
   const handleClick = () => {
     handleChoice(card);
   };
 
   return (
     <div className="card">
-      <img className="front" src={card.src} alt="card-front" />
-      <img
-        className="back"
-        src="./img/gift.png"
-        alt="card-back"
-        onClick={handleClick}
-      />
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card-front" />
+        <img
+          className="back"
+          src="./img/gift.png"
+          alt="card-back"
+          onClick={handleClick}
+        />
+      </div>
     </div>
   );
 };
